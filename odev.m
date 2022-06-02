87deea47fe71d9d136be40bbc36e93b785dff7c1
Alperen ARSLAN <105153770+ARSAlperen@users.noreply.github.com>
2022-06-02 23:04:58 +0300

    Update odev.m

--- odev.m
+++ odev.m
@@ -3,23 +3,6 @@
 clc;
 clear;
 
-% g1 = [2.9736, 0.9448, 1.6394, 0.0389, 1.2958]
-% g2 = [0.7681, 0.8027, 0.2156, 0.074, 1.5076]
-% g3 = [4.8249, 2.2516, 1.5609, 2.0452, 1.0959]
-% numperm = 10000
-% k=3
-% n1 = length(g1)
-% n2 = length(g2)
-% n3 = length(g3)
-% N = n1 + n2 + n3
-% SST = (n1*mean(g1)^2 + n2*mean(g2)^2 + n3*mean(g3)^2) - N*mean(g)^2
-% SStotal = (N-1) * var(g)
-% SSE = SStotal - SST
-% df1 = k-1
-% df2 = N-k
-% MST = SST / df1
-% MSE = SSE / df2
-
 n=[3 5 8 10 12]; % 
 mu=[5 5 5 5 5 ];
 sigma=[1 1 1 1 1]; 
@@ -64,7 +47,7 @@ for i=1:ds % 4. adım
     x5p=xp(n4+n3n+n2+n1+1:N)
     
     SSx=n1*mean(x1)^2+n2*mean(x2)^2+n3*mean(x3)^2+n4*mean(x4)^2+n5*mean(x5)^2
-    SSX_per[i]=SSx %xg
+%     SSX_per[i]=SSx %xg
     SST_per=SSx-SS_per  %A
     SSE_per=SStotal-SST_per  %B
     F_per(i)=(SST_per/k-1)/(SSE_per/N-k) %A/B
@@ -77,10 +60,14 @@ end
 %     Tp(i)=mean(x1p)-mean(x2p); % (3.adım)
 % end
 
-p_SSX=mean(SSX_per>SSX_orj); % 5.adım
+% p_SSX=mean(SSX_per>SSX_orj); % 5.adım
 p_F=mean(F_per>F_orj)
+r=p_F<alfa
+
+print("Permutation F-Test","\n")
+print("F Test Değeri:",F_orj,"\n")
+print("SSX Değeri:",SSX_orj,"\n")
+print("Number of Permutation: ",ds,"\n")
 
 end
 
-
-% r=p<alfa;
